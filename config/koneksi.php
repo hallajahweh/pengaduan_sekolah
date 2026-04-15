fc922039edb1616dbf7e9575c7870c8e9c427ae7
Halimah <halimah@gmail.com>
2026-04-16 06:40:09 +0700

    fix railway mysql env

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,13 +1,13 @@
 <?php
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+
+$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$pass = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$db   = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+$_ENV = getenv();
+
 require_once "router.php";
